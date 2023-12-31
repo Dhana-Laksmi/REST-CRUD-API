5cead2e16536ee93f76b9f0937382c92a59ec253
Dhana-Laksmi <deivadhanalakshmi@gmail.com>
2023-12-31 20:42:26 +0530

    Added get,delete,update api

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -26,4 +26,68 @@ async function createCity(data) {
     }
   }
 
-  module.exports={createCity}
+  async function getAllCity() {
+    try {
+      const cities = await cityRepository.getAll();
+      return cities;
+    } catch (error) {
+      throw new AppError(
+        "Cannot fetch all the data of cities",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+  async function getCity(id) {
+    try {
+      const city = await cityRepository.get(id);
+      return city;
+    } catch (error) {
+      if (error.statusCode == StatusCodes.NOT_FOUND) {
+        throw new AppError(
+          "The City you requested is not present",
+          error.statusCode
+        );
+      }
+      throw new AppError(
+        "Cannot fetch or find the data from city",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async function destroyCity(id) {
+    try {
+      const response = await cityRepository.destroy(id);
+      return response;
+    } catch (error) {
+      if (error.statusCode == StatusCodes.NOT_FOUND) {
+        throw new AppError(
+          "The city you requested to delete is not present",
+          error.statusCode
+        );
+      }
+      throw new AppError(
+        "Cannot fetch or find the data from city",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  async function updateCity(id, data) {
+    try {
+      const response = await cityRepository.update(id, data);
+      return response;
+    } catch (error) {
+      if (error.statusCode == StatusCodes.NOT_FOUND) {
+        throw new AppError(
+          "The city you requested is not present",
+          error.statusCode
+        );
+      }
+      throw new AppError(
+        "Cannot fetch or find the data from city",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+  module.exports={createCity,getAllCity,getCity,destroyCity,updateCity}
